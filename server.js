9a03c806fedab10e2cf290b984e8c760b991966f
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-07-05 14:16:59 -0400

    added .gitignore for the node_modules and fixed the code on server for better reading and organization

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/notes", (req, res) => {
@@ -14,50 +15,30 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "db.json"), "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Internal server error" });
-    }
-
-    const notes = JSON.parse(data);
-    res.json(notes);
-  });
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.status(200).json(`${req.method} request received to get notes`);
+  console.info(`${req.method} request received to get notes`);
 });
 
 app.post("/api/notes", (req, res) => {
-  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (err, data) => {
+  console.info(`${req.method} request received to add a note`);
+  const newNote = req.body;
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: "Internal server error" });
+    } else {
+      const notes = JSON.parse(data);
+      notes.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (writeErr) =>
+        writeErr
+          ? console.error(writeErr)
+          : console.info("Successfully updated reviews!")
+      );
     }
-
-    const notes = JSON.parse(data);
-    const newNote = req.body;
-    newNote.id = generateUniqueId();
-    notes.push(newNote);
-
-    fs.writeFile(
-      path.join(__dirname, "db.json"),
-      JSON.stringify(notes),
-      (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: "Internal server error" });
-        }
-
-        res.json(newNote);
-      }
-    );
   });
 });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-function generateUniqueId() {}
