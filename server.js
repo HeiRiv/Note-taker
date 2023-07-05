8d0de4b20f5598fab2f9e63c79af6d7b10ee6a90
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-07-05 15:29:41 -0400

    added delete button functionality and debugged the code so the get will work properly

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { stringify } = require("querystring");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -15,14 +16,26 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("/api/notes", (req, res) => {
-  res.status(200).json(`${req.method} request received to get notes`);
   console.info(`${req.method} request received to get notes`);
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to read notes" });
+    } else {
+      const notes = JSON.parse(data);
+      res.status(200).json(notes);
+    }
+  });
 });
 
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a note`);
-  const newNote = req.body;
-
+  const newNote = {
+    id: Math.floor(Math.random() * 9999),
+    title: req.body.title,
+    text: req.body.text,
+  };
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
@@ -35,6 +48,27 @@ app.post("/api/notes", (req, res) => {
           ? console.error(writeErr)
           : console.info("Successfully updated reviews!")
       );
+      res.json(notes);
+    }
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let notes = JSON.parse(data) || [];
+      notes = notes.filter((singlenote) => singlenote.id != req.params.id);
+
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (writeErr) =>
+        writeErr
+          ? console.error(writeErr)
+          : console.info("Successfully updated reviews!")
+      );
+      res.json(notes);
     }
   });
 });
